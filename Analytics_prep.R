186deb22013e3e7c0d6cf1753a59d52cf3c46360
unknown <liberrenaud@gmail.com>
2020-11-11 21:46:31 +0100

    Push _ lolipop chart - function not yet working (facet issue)

--- Analytics_prep.R
+++ Analytics_prep.R
@@ -1,9 +1,20 @@
 library(tidyverse)
 library(janitor)
 library(tidytext)
+library(scales)
+
+theme_set(theme_light())
 
 
-HR_raw_data <- readxl::read_xlsx("HR_Data.xlsx")
+scale_color <- scale_color_gradient2(midpoint=0, 
+                                     low="red",
+                                     mid="white",
+                                     high="blue", 
+                                     space ="Lab" )
+
+
+
+  HR_raw_data <- readxl::read_xlsx("HR_Data.xlsx")
 
 HR_raw_data %>% glimpse()
 
@@ -19,7 +30,7 @@ HR_clean_data <- HR_raw_data %>%
               dplyr::mutate(interview="2nd")
             )
 
-
+HR_clean_data %>% view()
 
 NPS_intro <- HR_clean_data %>% 
   dplyr::filter(interview=="2nd") %>% 
@@ -28,25 +39,41 @@ NPS_intro <- HR_clean_data %>%
   ungroup() %>% 
   pivot_wider(values_from = n,names_from=interview_outcome,values_fill=0) %>% 
   mutate(all_respondents=Happy+Neutral+Unhappy,
-         NPS=Happy/all_respondents-Unhappy/all_respondents)
+         NPS=Happy/all_respondents-Unhappy/all_respondents) %>% 
+  group_by(employee_region) %>% 
+  mutate(NPS_mean=sum(NPS)/nrow(.)) %>% 
+  ungroup()
 
 
 NPS_intro_viz<- function(wrap_category=employee_region,y_category=consultation_name) {
 
 NPS_intro %>% 
-  mutate(consultation_name=tidytext::reorder_within(consultation_name,NPS,employee_region)) %>% 
-  ggplot(aes(NPS,consultation_name)) +
-  geom_segment( aes(x=0, xend=NPS, y=consultation_name, yend=consultation_name), color="skyblue") +
-  geom_point( color="blue", size=2, alpha=0.6)+
-  facet_wrap(~wrap_category,scales = "free_y",nrow = 1)+
-  scale_y_reordered()
+  mutate(consultation_name=tidytext::reorder_within({{y_category}},NPS,{{wrap_category}})) %>% 
+  ggplot(aes(NPS,{{y_category}})) +
+  geom_vline(xintercept = 0,color="gray69", linetype='dashed')+
+  geom_vline(aes(xintercept = NPS_mean),color="cornflowerblue")+
+  geom_segment( aes(x=0, xend=NPS, y={{y_category}}, yend={{y_category}}), color="black") +
+  geom_point( aes(colour=NPS), 
+              size=2, 
+              #colour="black",
+              #alpha=0.6
+              )+
+
+  facet_wrap(~employee_region,scales = "free_y",nrow = 1)+
+  scale_y_reordered()+  
+  scale_color+
+  theme(
+    panel.grid.major.y=element_blank()+
+    panel.grid.major.x=element_line(colour = "grey93")
+  )
+    
   
 }
 
 
 NPS_intro_viz()
 
-NPS_intro %>% 
+  NPS_intro %>% 
   mutate(consultation_name=tidytext::reorder_within(consultation_name,NPS,employee_region)) %>% 
   ggplot(aes(NPS,consultation_name)) +
   geom_segment( aes(x=0, xend=NPS, y=consultation_name, yend=consultation_name), color="skyblue") +
