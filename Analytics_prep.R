356384406ac02608a629bb102f4d2674788f0ef7
unknown <liberrenaud@gmail.com>
2020-11-12 00:10:06 +0100

    Set up function + cleaning elements of scripts

--- Analytics_prep.R
+++ Analytics_prep.R
@@ -3,9 +3,12 @@ library(janitor)
 library(tidytext)
 library(scales)
 
+install.packages("likert")
+install.packages("pisaitems")
+
 theme_set(theme_light())
 
-
+# Set scale for the lolipop graph
 scale_color <- scale_color_gradient2(midpoint=0, 
                                      low="red",
                                      mid="white",
@@ -13,11 +16,11 @@ scale_color <- scale_color_gradient2(midpoint=0,
                                      space ="Lab" )
 
 
+# Import Data
+HR_raw_data <- readxl::read_xlsx("HR_Data.xlsx")
 
-  HR_raw_data <- readxl::read_xlsx("HR_Data.xlsx")
 
-HR_raw_data %>% glimpse()
-
+# Clean the data
 HR_clean_data <- HR_raw_data %>% 
   janitor::clean_names() %>% 
   dplyr::select(employee_id:x1st_interview_outcome) %>% 
@@ -30,63 +33,116 @@ HR_clean_data <- HR_raw_data %>%
               dplyr::mutate(interview="2nd")
             )
 
-HR_clean_data %>% view()
-
-NPS_intro <- HR_clean_data %>% 
-  dplyr::filter(interview=="2nd") %>% 
-  group_by(employee_region, consultation_name,interview_outcome) %>% 
-  summarise(n=n()) %>% 
-  ungroup() %>% 
-  pivot_wider(values_from = n,names_from=interview_outcome,values_fill=0) %>% 
-  mutate(all_respondents=Happy+Neutral+Unhappy,
-         NPS=Happy/all_respondents-Unhappy/all_respondents) %>% 
-  group_by(employee_region) %>% 
-  mutate(NPS_mean=sum(NPS)/nrow(.)) %>% 
-  ungroup()
-
-
+# Function - Intro Lollipop chart
 NPS_intro_viz<- function(wrap_category=employee_region,y_category=consultation_name) {
+  
+  
+  NPS_intro <- HR_clean_data %>% 
+    dplyr::filter(interview=="2nd") %>% 
+    group_by({{wrap_category}}, {{y_category}},interview_outcome) %>% 
+    summarise(n=n()) %>% 
+    ungroup() %>% 
+    pivot_wider(values_from = n,names_from=interview_outcome,values_fill=0) %>% 
+    mutate(all_respondents=Happy+Neutral+Unhappy,
+           NPS=Happy/all_respondents-Unhappy/all_respondents) %>% 
+    group_by({{wrap_category}}) %>% 
+    mutate(NPS_mean=sum(NPS)/nrow(.)) %>% 
+    ungroup()
 
-NPS_intro %>% 
-  mutate(consultation_name=tidytext::reorder_within({{y_category}},NPS,{{wrap_category}})) %>% 
-  ggplot(aes(NPS,{{y_category}})) +
-  geom_vline(xintercept = 0,color="gray69", linetype='dashed')+
-  geom_vline(aes(xintercept = NPS_mean),color="cornflowerblue")+
-  geom_segment( aes(x=0, xend=NPS, y={{y_category}}, yend={{y_category}}), color="black") +
-  geom_point( aes(colour=NPS), 
-              size=2, 
-              #colour="black",
-              #alpha=0.6
-              )+
-
-  #ISSUE IN MY FACET _ DID NOT MANAGE TO TIDYEVAL IT
-  facet_wrap(~employee_region,scales = "free_y",nrow = 1)+
-  scale_y_reordered()+  
-  scale_color+
-  theme(
-    panel.grid.major.y=element_blank()+
-    panel.grid.major.x=element_line(colour = "grey93")
-  )
+  NPS_intro %>% 
+    mutate(consultation_name=tidytext::reorder_within({{y_category}},NPS,{{wrap_category}})) %>% 
+    ggplot(aes(NPS,{{y_category}})) +
+    geom_vline(xintercept = 0,color="gray69", linetype='dashed')+
+    geom_vline(aes(xintercept = NPS_mean),color="cornflowerblue")+
+    geom_segment( aes(x=0, xend=NPS, y={{y_category}}, yend={{y_category}}), color="black") +
+    geom_point( aes(colour=NPS), 
+                size=2, 
+                #colour="black",
+                #alpha=0.6
+                )+
+  
+    #ISSUE IN MY FACET _ DID NOT MANAGE TO TIDYEVAL IT
+  
+    scale_y_reordered()+  
+    scale_color+
+    theme(
+      panel.grid.major.y=element_blank(),
+      panel.grid.major.x=element_line(colour = "grey93")
+    )
     
   
 }
 
 
-NPS_intro_viz()
 
-  NPS_intro %>% 
-  mutate(consultation_name=tidytext::reorder_within(consultation_name,NPS,employee_region)) %>% 
-  ggplot(aes(NPS,consultation_name)) +
-  geom_segment( aes(x=0, xend=NPS, y=consultation_name, yend=consultation_name), color="skyblue") +
-  geom_point( color="blue", size=2, alpha=0.6)+
-  facet_wrap(~employee_region,scales = "free_y",nrow = 1)+
-  scale_y_reordered()
+
+
+NPS_intro_viz()+
+facet_wrap(~employee_region,scales = "free_y",nrow = 1)
+
+
+NPS_intro_viz(wrap_category=consultation_name,y_category=employee_region,y_label="Regions")+
+  facet_wrap(~consultation_name,scales = "free_y",nrow = 1)
+
   
   
-    
+
+
   
 
   
 
 HR_clean_data %>% glimpse()      
 
+
+
+
+
+BACK UP
+# 
+# 
+# NPS_intro_viz<- function(wrap_category=employee_region,y_category=consultation_name,y_label="Metrics") {
+#   
+#   # Prepare the data for the visualization
+#   NPS_intro <- HR_clean_data %>% 
+#     dplyr::filter(interview=="2nd") %>% 
+#     dplyr::group_by({{wrap_category}}, {{y_category}},interview_outcome) %>% 
+#     dplyr::summarise(n=n()) %>% 
+#     dplyr::ungroup() %>% 
+#     tidyr::pivot_wider(values_from = n,names_from=interview_outcome,values_fill=0) %>% 
+#     dplyr::mutate(all_respondents=Happy+Neutral+Unhappy,
+#                   NPS=round((Happy/all_respondents-Unhappy/all_respondents)*100),0) %>% 
+#     dplyr::group_by({{wrap_category}}) %>% 
+#     dplyr::mutate(NPS_mean=sum(NPS)/nrow(.)) %>% 
+#     dplyr::ungroup()%>% 
+#     dplyr::mutate(y_names=tidytext::reorder_within({{y_category}},NPS,{{wrap_category}}))
+#   
+#   # Visualization of the data
+#   NPS_intro %>% 
+#     #Set up Geoms
+#     ggplot(aes(NPS,y_names)) +
+#     geom_vline(xintercept = 0,color="gray69", linetype='dashed')+
+#     geom_vline(aes(xintercept = NPS_mean),color="cornflowerblue")+
+#     geom_segment( aes(x=0, xend=NPS, y=y_names, yend=y_names), color="black") +
+#     geom_point( aes(colour=NPS), 
+#                 size=2, 
+#                 #colour="black",
+#                 #alpha=0.6
+#     )+
+#     
+#     #ISSUE IN MY FACET _ DID NOT MANAGE TO TIDYEVAL IT
+#     #facet_wrap(~consultation_name,scales = "free_y",nrow = 1)
+#     
+#     #Set up formatting and labs
+#     scale_y_reordered()+  
+#     scale_color+
+#     theme(
+#       panel.grid.major.y=element_blank(),
+#       panel.grid.major.x=element_line(colour = "grey93")
+#     )+
+#     labs(title = "NPS results by Region and Metrics",
+#          x = "NPS Score",
+#          y = y_label)
+#   
+#   
+# }
